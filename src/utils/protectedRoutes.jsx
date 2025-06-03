16dcda83e54767b4b4d84d5d47cc07d21b1a99be
Ming Li <mingrkli@gmail.com>
2025-06-02 21:27:44 -0700

    Cap plz?

--- src/utils/protectedRoutes.jsx
+++ src/utils/protectedRoutes.jsx
@@ -12,47 +12,23 @@ function ProtectedRoutes() {
 
   return (
     <div>
-      { loggedIn ? 
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Dashboard/>} 
-            />
-            <Route 
-              path="/Dashboard" 
-              element={<Dashboard/>} 
-            />
-            <Route 
-              path="/SpecimensExplorer" 
-              element={<SpecimensExplorer/>} 
-            />
-            <Route 
-              path="/specimen/:id" 
-              element={<SpecimenDetail/>} 
-            />
-            <Route 
-              path="/AddArtifact" 
-              element={<AddArtifact/>} 
-            />
-            <Route 
-              path="/UpdateProduct/:id" 
-              element={<UpdateProduct/>} 
-            />
-            <Route path="/LoginForm" element={<LoginForm/>}/>
-            <Route 
-              path="/Logout" 
-              element={<Logout/>} 
-            />
-          </Routes>
-        : 
-          <Routes>
-            <Route path="/LoginForm" element={<LoginForm />} />
-            <Route 
-              path="*" 
-              element={<Navigate to='/LoginForm' replace />}
-            />
-          </Routes>
-      }
+      {loggedIn ? (
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/SpecimensExplorer" element={<SpecimensExplorer />} />
+          <Route path="/specimen/:id" element={<SpecimenDetail />} />
+          <Route path="/AddArtifact" element={<AddArtifact />} />
+          <Route path="/UpdateProduct/:id" element={<UpdateProduct />} />
+          <Route path="/LoginForm" element={<LoginForm />} />
+          <Route path="/Logout" element={<Logout />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/LoginForm" element={<LoginForm />} />
+          <Route path="*" element={<Navigate to="/LoginForm" replace />} />
+        </Routes>
+      )}
     </div>
   )
 }
